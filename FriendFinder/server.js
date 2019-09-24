a1e5f33ca59c40fb6114c1547c9403cd18e4307d
Tahsin Nazmee <tnazmee@yahoo.com>
2019-09-24 18:36:38 -0500

    changed to another server

--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var path = require('path');
 
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 1625;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
